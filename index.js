3cfee207a300e6e493b3f9bd7e28090f86b416b2
Manan Jain <writetomananjain21@gmail.com>
2024-08-01 01:02:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,6 +47,13 @@ app.post("/chats",(req,res)=>{
     res.redirect("/chats");
 });
 
+//Edit Route
+app.get("/chats/:id/edit",async(req,res)=>{
+    let{id}=req.params;
+    let chat=await Chat.findById(id);
+    res.render("edit.ejs",{chat});
+});
+
 app.listen(8080,()=>{
     console.log("Server is working");
 })
